9c3ad134a9da7daacdb852b6f80d76971ca5cae8
Lars Kluge <l@larskluge.com>
2017-04-04 11:41:12 +0200

    .

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	avro "github.com/elodina/go-avro"
+	avroKafka "github.com/elodina/go-kafka-avro"
+)
+
+var schemaFlag = flag.String("schema", "http://schema:8081", "Schema Registry URL")
+
+func main() {
+	flag.Parse()
+
+	decoder := avroKafka.NewKafkaAvroDecoder(*schemaFlag)
+
+	in, err := ioutil.ReadAll(os.Stdin)
+	check(err)
+
+	decoded, err := decoder.Decode(in)
+	check(err)
+
+	rec := decoded.(*avro.GenericRecord)
+	fmt.Println(rec)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
